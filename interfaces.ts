24326ac9027bc7f3be2a44a60c7327095978b98e
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-05-23 10:27:55 +0700

    refactor

---
+++ interfaces.ts
@@ -0,0 +1,73 @@
+interface IShopeeTrendDataSectionIndex {
+  data_type: string;
+  key: string;
+  filtered?: string;
+  filtered_dunit?: string;
+}
+
+interface IShopeeTrendDataSectionDataTopProduct {
+  info: string;
+  count: number;
+  data_type: string;
+  name: string;
+  label: string;
+  key: string;
+  images: string[];
+  list?: string[];
+}
+
+interface IShopeeTrendDataSectionData {
+  item?: string;
+  keyword: string[];
+  ads?: string;
+  top_product: IShopeeTrendDataSectionDataTopProduct[];
+  collection?: string;
+  item_lite?: string;
+  video?: string;
+  voucher?: string;
+  voucher_detail?: string;
+  l1cat?: string;
+  collection_lite?: string;
+  l2cat?: string;
+  shop?: string;
+  shop_lite?: string;
+  shopcat?: string;
+  feed?: string;
+  feed_tab?: string;
+  stream?: string;
+  promotion?: string;
+  knode?: string;
+  food_item?: string;
+}
+
+interface IShopeeTrendDataSection {
+  total: number;
+  key: string;
+  index: IShopeeTrendDataSectionIndex[];
+  data: IShopeeTrendDataSectionData;
+  item_card_type: string;
+}
+
+interface IShopeeTrendData {
+  update_time: number;
+  version: string;
+  sections: IShopeeTrendDataSection[];
+  expire: number;
+  tab_meta_data?: string;
+  misc_info?: string;
+}
+
+interface IShopeeTrendResponse {
+  bff_meta?: string;
+  error: number;
+  error_msg?: string;
+  data: IShopeeTrendData;
+}
+
+interface trend {
+  keyword: string;
+  image: string;
+  count: number;
+  source: string;
+  timestamp: string;
+}
--- postprocess.ts
+++ postprocess.ts
@@ -1,78 +1,6 @@
-import { readJSON, writeJSON } from "https://deno.land/x/flat/mod.ts";
-
-interface IShopeeTrendDataSectionIndex {
-  data_type: string;
-  key: string;
-  filtered?: string;
-  filtered_dunit?: string;
-}
-
-interface IShopeeTrendDataSectionDataTopProduct {
-  info: string;
-  count: number;
-  data_type: string;
-  name: string;
-  label: string;
-  key: string;
-  images: string[];
-  list?: string[];
-}
-
-interface IShopeeTrendDataSectionData {
-  item?: string;
-  keyword: string[];
-  ads?: string;
-  top_product: IShopeeTrendDataSectionDataTopProduct[];
-  collection?: string;
-  item_lite?: string;
-  video?: string;
-  voucher?: string;
-  voucher_detail?: string;
-  l1cat?: string;
-  collection_lite?: string;
-  l2cat?: string;
-  shop?: string;
-  shop_lite?: string;
-  shopcat?: string;
-  feed?: string;
-  feed_tab?: string;
-  stream?: string;
-  promotion?: string;
-  knode?: string;
-  food_item?: string;
-}
-
-interface IShopeeTrendDataSection {
-  total: number;
-  key: string;
-  index: IShopeeTrendDataSectionIndex[];
-  data: IShopeeTrendDataSectionData;
-  item_card_type: string;
-}
-
-interface IShopeeTrendData {
-  update_time: number;
-  version: string;
-  sections: IShopeeTrendDataSection[];
-  expire: number;
-  tab_meta_data?: string;
-  misc_info?: string;
-}
-
-interface IShopeeTrendResponse {
-  bff_meta?: string;
-  error: number;
-  error_msg?: string;
-  data: IShopeeTrendData;
-}
-
-interface trend {
-  keyword: string;
-  image: string;
-  count: number;
-  source: string;
-  timestamp: string;
-}
+import { readJSON } from "https://deno.land/x/flat/mod.ts";
+import { IShopeeTrendResponse, ITrend } from "./interfaces.ts";
+import { appendJSON } from "https://raw.githubusercontent.com/siral-id/deno-utility/main/utility.ts";
 
 const filename = Deno.args[0]; // Same name as downloaded_filename
 const response: IShopeeTrendResponse = await readJSON(filename);
@@ -84,7 +12,7 @@ if (response["error"] !== 0) {
 const section = response["data"]["sections"][0];
 const products = section["data"]["top_product"];
 
-const currentTrends: trend[] = products.map(({ name, images, count }) => {
+const trends: ITrend[] = products.map(({ name, images, count }) => {
   const image = `https://cf.shopee.co.id/file/${images[0]}`;
   const timestamp = new Date().toISOString();
 
@@ -98,8 +26,4 @@ const currentTrends: trend[] = products.map(({ name, images, count }) => {
 });
 
 const output = `shopee_trends.json`;
-const existingTrends = await readJSON(output);
-
-const trends = existingTrends.concat(currentTrends)
-
-await writeJSON(output, trends);
+await appendJSON(output, trends);
