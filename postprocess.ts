b1c84cee22c53fc96d926628c4c8c6aa50c40790
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-08 15:29:25 +0700

    chore: change repo path

--- postprocess.ts
+++ postprocess.ts
@@ -39,7 +39,7 @@ const uuid = v4.generate();
 
 await octokit.rest.issues.create({
   owner: "siral-id",
-  repo: "database",
+  repo: "core",
   title: `WRITE_TRENDS_SHOPEE_${uuid}`,
   body: JSON.stringify(trends),
 });
