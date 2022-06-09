3ce56b3a86033f6843443598af9140bf75475aad
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-09 20:51:42 +0700

    chore: move octokit

--- postprocess.ts
+++ postprocess.ts
@@ -1,16 +1,9 @@
-import { Octokit } from "https://cdn.skypack.dev/octokit@v1.7.2?dts";
 import { readJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
-import { v4 } from "https://deno.land/std@0.142.0/uuid/mod.ts";
 import { IShopeeTrendResponse } from "./mod.ts";
 import {
   ICreateTrend,
   Source,
 } from "https://raw.githubusercontent.com/siral-id/core/main/mod.ts";
-
-export function setupOctokit(ghToken?: string): Octokit {
-  if (!ghToken) throw new Error("GH_TOKEN not found");
-  return new Octokit({ auth: ghToken });
-}
 
 export async function loadResponse(
   filename: string,
@@ -35,16 +28,5 @@ export function serializeResponse(
       source: Source.SHOPEE,
       timestamp,
     };
-  });
-}
-
-export async function upload(octokit: Octokit, data: ICreateTrend[]) {
-  const uuid = v4.generate();
-
-  await octokit.rest.issues.create({
-    owner: "siral-id",
-    repo: "core",
-    title: `WRITE_TRENDS_SHOPEE_${uuid}`,
-    body: JSON.stringify(data),
   });
 }
