630e4847179c75187b7b2906b2ad3eca0f83fde3
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-12 17:27:14 +0700

    bugfix: add withRetry

--- cli.ts
+++ cli.ts
@@ -2,6 +2,7 @@ import { loadResponse, serializeResponse } from "./mod.ts";
 import {
   ICreateTrend,
   setupOctokit,
+  sleep,
   upload,
 } from "https://raw.githubusercontent.com/siral-id/core/main/mod.ts";
 
@@ -13,6 +14,25 @@ const response = await loadResponse(filename);
 
 const serializedData = serializeResponse(response);
 
-await upload<ICreateTrend[]>(octokit, serializedData, "WRITE_SHOPEE_TRENDS");
+const uploadWithRetry = async <T>(
+  data: T,
+  retryCount = 0,
+  maxRetry = 60,
+  lastError?: string,
+): Promise<void> => {
+  if (retryCount > maxRetry) throw new Error(lastError);
+  try {
+    await upload<T>(
+      octokit,
+      data,
+      "WRITE_SHOPEE_TRENDS",
+    );
+  } catch (error) {
+    await sleep(retryCount);
+    await uploadWithRetry(data, retryCount + 1, error);
+  }
+};
+
+await uploadWithRetry<ICreateTrend[]>(serializedData);
 
 Deno.exit();
